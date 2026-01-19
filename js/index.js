21d8ad8264d947f63bd569c923ddb6a8a8188d9a
Root <118435307+Rootm2741@users.noreply.github.com>
2026-01-20 01:10:22 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+btn.addEventListener('click', (event) => {
+    text.textContent = "아무 일도 없었다."
+});
+console.log('Test script loaded');
+alert('Test script executed');
